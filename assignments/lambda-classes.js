2f193ef261815a634806068edd8d5f69826c84ff
Eric Hendrix <agentmrbig@gmail.com>
2019-04-04 22:43:09 -0400

    Started the lambda-classes.js file, I completed creating the Person class

--- assignments/lambda-classes.js
+++ assignments/lambda-classes.js
@@ -1,1 +1,51 @@
 // CODE here for your Lambda Classes
+// class GameObject{
+//     constructor(attrs){
+//         this.createdAt = attrs.createdAt;
+//         this.name = attrs.name;
+//         this.dimensions = attrs.dimensions;
+//     }
+//     destroy(){
+//         return `${this.name} was removed from the game.`;
+//     }
+// }
+
+
+
+class Person{
+    constructor(attrs){
+        this.name = attrs.name;
+        this.age = attrs.age;
+        this.location = attrs.location;
+        this.gender = attrs.gender;
+
+    }
+    speak(){
+        `Hello my name is ${this.name}, I am from ${this.location}`
+    }
+}
+
+class Instructor extends Person{
+    constructor(attrs){
+        super(attrs);
+        this.specialty = attrs.specialty;
+        this.favLanguage = attrs.favLanguage;
+        this.catchPhrase = attrs.catchPhrase;
+    }
+    demo(subject){
+        return `Today we are learning about ${subject}`;
+    }
+    grade(student, subject){
+        return `${student.name} receives a perfect score on ${subject}`;
+    }
+}
+// class CharacterStats extends GameObject{
+//     constructor(attrs){
+//         super(attrs);
+//         this.healthPoints = attrs.healthPoints;
+//     }
+//     takeDamage(){
+//         return `${this.name} took damage.`;
+//     }
+// }
+
